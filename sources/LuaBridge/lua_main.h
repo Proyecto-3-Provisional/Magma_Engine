2ef1e6a65e8473587b8e6561b5256a2f227bee23
jrubio05 <jrubio05@ucm.es>
2023-04-26 00:58:29 +0200

    Lectura de escena con Lua: mapa de mapas
    🌕

--- sources/LuaBridge/lua_main.h
+++ sources/LuaBridge/lua_main.h
@@ -2,17 +2,6 @@
 
 #include <string>
 
-#define NUM_P_CMPS 6
-
-//incluir .lua detras del nombre que se inserte
+//incluir extensión (.lua, .magmascene, etc.)
+//detrás del nombre que se inserte
 int luaMain(std::string filename);
-
-const std::string possibleCmps[NUM_P_CMPS] = {
-	"Transform",
-	"Mesh",
-	"Rigidbody",
-	"Image",
-	"Text",
-	"Button"
-	//AudioSource
-};
