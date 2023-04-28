59422b19b9aa869b7e25ca14d8062eda343c0b0f
pablos16 <pablos16@ucm.es>
2023-04-28 10:31:09 +0200

    subido comienzo componente size para testeo en el motor

---
+++ sources/EC/size_temp.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <EC/component.h>
+
+namespace magma_engine
+{
+	class Rigidbody;
+	
+	class Size_Temp : public ec::Component
+	{
+	public:
+		Size_Temp();
+		virtual ~Size_Temp();
+
+		virtual bool initComponent();
+		void update(float deltaTime);
+
+		float tam = 1.0f;
+	};
+}
